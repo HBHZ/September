e111ae725899946ff45c91f8fc99b2f1631e952f
amirong <amirong@172.20.10.3>
2017-09-14 07:22:56 +0800

    Json与Array，Object的关系；
    php路由的原理

--- newfile.php
+++ newfile.php
@@ -1,91 +1,38 @@
 <?php
 
-require_once ('Class_Core.php');
-
-$value = new Core();
-
-$value = serialize($value);
-
-echo $value;
-
-echo "</br>";
-
-echo "</br>";
-
-$value = json_encode(serialize($value),JSON_OBJECT_AS_ARRAY);
-
-echo $value;
-
-$value = serialize(123);
-
-echo "</br>";
-
-echo "</br>";
-
-echo $value;
-
-$value = json_encode(serialize(123),JSON_FORCE_OBJECT);
-
-
-echo "</br>";
-
-echo "</br>";
-
-echo $value;
-
-
-echo "</br>";
-
-echo "</br>";
-
-$array = ['€', 'http://example.com/some/cool/page', '337'];
-$bad   = json_encode($array);
-
-
-echo $bad;
-echo "</br>";
-
-echo "</br>";
-
-$good  = json_encode($array,  JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK);
-
-echo $good;
+$json = '{"a":1,"b":2,"c":3,"d":4,"e":5}';
+var_dump(json_decode($json));
+var_dump(json_decode($json, true));
 
 
 
 
-$name = "123";
-echo <<<EOT
-            <html>
-            <head>
-            <meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
-            <title>Untitled Document</title>
-            </head>
-            <body>
-            <!--12321-->
-            <a href="baidu.com">Hello,$name!</a>
-            </body>
-            </html>
-EOT;
 
-$file = file('./newfile.php');
+$a = array('<foo>',"'bar'",'"baz"','&blong&', "\xc3\xa9");
+
+echo "Normal: ", json_encode($a), "\n";
+echo "Tags: ", json_encode($a, JSON_HEX_TAG), "\n";
+echo "Apos: ", json_encode($a, JSON_HEX_APOS), "\n";
+echo "Quot: ", json_encode($a, JSON_HEX_QUOT), "\n";
+echo "Amp: ", json_encode($a, JSON_HEX_AMP), "\n";
+echo "Unicode: ", json_encode($a, JSON_UNESCAPED_UNICODE), "\n";
+echo "All: ", json_encode($a, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE), "\n\n";
+
+$b = array();
+
+echo "Empty array output as array: ", json_encode($b), "\n";
+echo "Empty array output as object: ", json_encode($b, JSON_FORCE_OBJECT), "\n\n";
+
+$c = array(array(1,2,3));
+
+echo "Non-associative array output as array: ", json_encode($c), "\n";
+echo "Non-associative array output as object: ", json_encode($c, JSON_FORCE_OBJECT), "\n\n";
+
+$d = array('foo' => 'bar', 'baz' => 'long');
+
+echo "Associative array always output as object: ", json_encode($d), "\n";
+echo "Associative array always output as object: ", json_encode($d, JSON_FORCE_OBJECT), "\n\n"; 
 
 
-$count = array_sum($file);
-echo "</br>";
-
-echo "</br>";
-$contents = file_get_contents('./newfile.php');
-
-
-echo $contents;
-
-
-echo "</br>";
-
-echo "</br>";
-echo $count;
- list($filename) = $file;
- 
- echo $filename;
+//下一个问题路由的设计
 
