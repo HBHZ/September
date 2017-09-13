3236426b707eb229f45ecec661bc30753757105d
amirong <amirong@172.20.10.3>
2017-09-14 07:52:19 +0800

    朋友列表获取

--- friend.php
+++ friend.php
@@ -1,108 +1,139 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>无标题文档</title>
-<style type="text/css">
-*
-{
-    margin:0px auto; 
-    padding:0px; 
-    font-family:"微软雅黑";    
-}
-#list
-{
-    width:350px;
-    height:400px;
-    background-color:#999;
-        
-}
-.py
-{
-    width:350px; height:40px;    
-    margin:8px 0px 0px 0px;
-}
-.py:hover
-{
-    background-color:#3F0;
-    color:#FFF;
-    cursor:pointer;
-}
-.img
-{
-    width:35px; height:40px; float:left;
-}
-.nc
-{
-    float:left; height:40px;
-    margin:0px 0px 0px 20px;
-    line-height:40px;
-    vertical-align:middle;
-}
-</style>
 
-</head>
-
-<body>
 <?php
-    $uid = "15053397521";  //因为没有从登陆页面传过来,所以先给uid传一个值
-?>
-
-<div id="list">
-    <?php
-    //造链接对象
-    $db =new MySQLi("localhost","root","517","weixin");
-    //判断是否出错
-    !mysqli_connect_error() or die("连接失败");
-    //写sql语句
-    $sql = "select Friends from friends where Uid='{$uid}'";
-    //执行sql语句
-    $result = $db->query($sql);
-    
-    $attr = $result->fetch_all();
-    
-    for($i=0;$i<count($attr);$i++)
+error_reporting(7);
+class msn
+{
+    private $startcomm = 0;
+    private $username = '';
+    private $password = '';
+    private $commend = '';
+    private $domain = '';
+    private $socket = '';
+    private $challenge = '';
+    private $status = array();
+    private $data = array();
+    function set_account($username, $password)
     {
-        //查出朋友的用户名，因为二维数组的结果就一个值,所以索引取0.
-        $fuid = $attr[$i][0];
-        //查users表,根据朋友的UID查出朋友的昵称和头像
-        $sqlf = "select Pic,NickName from Users where Uid='{$fuid}'";
-        $resultf = $db->query($sqlf);
-        
-        $attrf = $resultf->fetch_row();//因为是根据朋友的uid查询的，所以查出的只能是一条数据,所以最好用fetch_row()方便些
-        
-        //在外层div里加一个bs自定义属性,方便后期加功能,想选中某一个DIV的时候,存上他的用户名,以后方便以后取出
-        echo"<div onclick='ShowCode(this)' class='py' bs='{fuid}'>
-        <img class='img' src='{$attrf[0]}' />
-        <div class='nc'>{$attrf[1]}</div>
-        </div>";
-        
-        
+        $this->username = $username;
+        $this->password = $password;
     }
-    
-?>
-</div>
-<!--用js的方式更改样式-->
-<script type="text/javascript">
- function ShowCode(div)
- {
-     //让div在被选中之前,先把之前所有div的样式先清除掉
-         var a = document.getElementsByClassName("py"); //查出来的a是一个集合,js里的集合和PHP里的数组是一样的道理
-        //利用for循环把所有div的样式清除
-        for(var i=0;i<d.length;i++)
-        {
-            //alert(d[i]);         //可以alert试试,看下输出的是什么.如果输出的是divelement,就说明找到了每一个DIV了.d[i]就可以代表div了.
-            d[i].style.backgroundcolor = "#FFF";
-            d[i].style.color = "#000";
+
+    function getData(){
+        $buffer="";
+        while (!feof($this->socket)) {
+            $buffer .= fread($this->socket,1024);
+            if (preg_match("//r/",$buffer)) {
+                break;
+            }
         }
-        //清除之后再给选中的div加上指定的样式
-       div.style.backgroundcolor = "#3F0";   //修改样式的时候,背景色backgroundcolor中间没有横杠链接的
-       div.style.Color = "#FFF";                //修改字体颜色
-       
-       //每点击一个div让它显示输出谁的用户名
-       alert(div.getAttribute("bs"));   //把用户名存在自定义属性bs里了,所以可以通过这个方法获取属性
-     
- }
-</script>
-</body>
-</html>
+        $this->checkData($buffer);
+    }
+
+    function getData2() {
+        $buffer="";
+        while (!feof($this->socket)) {
+            $buffer .= fread($this->socket,1024);
+            if (preg_match("//r/n/r/n/",$buffer)) {
+                break;
+            }
+        }
+        $this->checkData($buffer);
+    }
+    function checkData($buffer) {
+        if (preg_match("/lc/=(.+?)/Ui",$buffer,$matches)) {    
+            $this->challenge = "lc=" . $matches[1];
+        }
+        if (preg_match("/(XFR 3 NS )([0-9/./:]+?) (.*) ([0-9/./:]+?)/is",$buffer,$matches)) {
+            $split = explode(":",$matches[2]);
+            $this->startcomm = 1;
+            $this->msn_connect($split[0],$split[1]);
+        }
+        if (preg_match("/tpf/=([a-zA-Z0-9]+?)/Ui",$buffer,$matches)) {
+            $this->nexus_connect($matches[1]);
+        }
+        $split = explode("/n",$buffer);
+        for ($i=0;$i<count($split);$i++) {  
+            $detail = explode(" ",$split[$i]);
+            if ($detail[0] == "LST") {
+                if(isset($detail[2])) $this->data[] = array($detail[1], urldecode($detail[2]));
+            }
+        }
+        $this->status = array(200, $this->data);
+        //echo $buffer;
+    }
+    function msn_connect($server,$port) {
+        if ($this->socket) {
+            fclose($this->socket);
+        }
+        $this->socket = @fsockopen($server,$port, $errno, $errstr, 20);
+        if (!$this->socket) {
+            $this->status = array(500,'MSN验证服务器无法连接');
+            return false;
+
+        } else {
+
+            $this->startcomm++;
+            $this->send_command("VER " . $this->startcomm . " MSNP8 CVR0",1);
+            $this->send_command("CVR " . $this->startcomm . " 0x0409 win 4.10 i386 MSNMSGR 6.2 MSMSGS " . $this->username,1);
+            $this->send_command("USR " . $this->startcomm . " TWN I " . $this->username,1);
+        }
+    }
+    function send_command($command) {
+        $this->commend = $command;
+        $this->startcomm++;       
+        fwrite($this->socket,$command . "/r/n");
+        $this->getData();
+    }
+    function nexus_connect($tpf) {
+        $arr[] = "GET /rdr/pprdr.asp HTTP/1.0/r/n/r/n";
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, "https://nexus.passport.com:443/rdr/pprdr.asp");
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_VERBOSE, 0);
+        curl_setopt($curl, CURLOPT_HEADER,1);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $arr);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        $data = curl_exec($curl);
+        curl_close($curl);
+        preg_match("/DALogin=(.+?),/",$data,$matches);
+        if(!isset($matches[1])) return false;
+        $split = explode("/",$matches[1]);
+        $headers[0] = "GET /$split[1] HTTP/1.1/r/n";
+        $headers[1] = "Authorization: Passport1.4 OrgVerb=GET,OrgURL=http%3A%2F%2Fmessenger%2Emsn%2Ecom,sign-in=" . $this->username . ",pwd=" . $this->password . ", " . trim($this->challenge) . "/r/n";
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, "https://" . $split[0] . ":443/". $split[1]);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_VERBOSE, 0);
+        curl_setopt($curl,CURLOPT_FOLLOWLOCATION,1);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_HEADER,1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        $data = curl_exec($curl);
+        curl_close($curl);
+        preg_match("/t=(.+?)'/",$data,$matches);
+        if(!isset($matches[1])){
+
+            $this->status = array(404, '你输入的MSN帐号或者密码错误');
+            return false;
+        }
+        $this->send_command("USR " . $this->startcomm . " TWN S t=" . trim($matches[1]) . "",2);
+        $this->send_command("CHG " . $this->startcomm . " HDN",2);
+        $this->send_command("SYN " . $this->startcomm . " 0",2);
+        $this->getData2();
+        $this->send_command("SYN " . $this->startcomm . " 1 46 2",2);
+        $this->getData2();
+        $this->send_command("CHG ". $this->startcomm . " BSY");
+        $this->getData();     
+    }
+    public function getStatus()
+    {
+        return $this->status;
+    }
+}
+
+$msn = new MSN;
+$msn->set_account('xx@hotmail.com', 'xxxxx');
+$msn->msn_connect("messenger.hotmail.com",1863);
+$data = $msn->getStatus();
+print_r($data);
+?>
